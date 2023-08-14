24f9372257df306e6d2d7a9f2ae6ed9416fec3d1
Karen Payne <paynekaren@comcast.net>
2023-08-14 03:34:10 -0700

    comments

--- DataGridViewSample/Classes/DataGridViewExtensions.cs
+++ DataGridViewSample/Classes/DataGridViewExtensions.cs
@@ -1,31 +1,35 @@
-﻿namespace DataGridViewSample.Classes
+﻿namespace DataGridViewSample.Classes;
+
+public static class DataGridViewExtensions
 {
-    public static class DataGridViewExtensions
+    /// <summary>
+    /// Properly expand each column in a DataGridView
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="sizable"></param>
+    public static void ExpandColumns(this DataGridView source, bool sizable = false)
     {
-        public static void ExpandColumns(this DataGridView source, bool sizable = false)
+        foreach (DataGridViewColumn col in source.Columns)
         {
-            foreach (DataGridViewColumn col in source.Columns)
+            if (col.ValueType.Name != "ICollection`1")
             {
-                if (col.ValueType.Name != "ICollection`1")
-                {
-                    col.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
-                }
+                col.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
             }
-
-            if (!sizable) return;
-
-            for (int index = 0; index <= source.Columns.Count - 1; index++)
-            {
-                int columnWidth = source.Columns[index].Width;
-
-                source.Columns[index].AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
-
-                // Set Width to calculated AutoSize value:
-                source.Columns[index].Width = columnWidth;
-            }
-
-
         }
 
+        if (!sizable) return;
+
+        for (int index = 0; index <= source.Columns.Count - 1; index++)
+        {
+            int columnWidth = source.Columns[index].Width;
+
+            source.Columns[index].AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+
+            // Set Width to calculated AutoSize value:
+            source.Columns[index].Width = columnWidth;
+        }
+
+
     }
+
 }
